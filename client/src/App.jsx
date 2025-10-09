128274d182fe98bfa1d14ca2f8510d44194103e5
Aswini S <aswiniskthvl@gmail.com>
2025-10-09 12:39:14 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
+import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/HotelOwner/Layout';
+import Dashboard from './pages/HotelOwner/Dashboard';
+import ListRoom from './pages/HotelOwner/ListRoom';
+import AddRoom from './pages/HotelOwner/AddRoom';
+
+const App = () => {
+const isOwnerpath = useLocation().pathname.includes("owner");
+  return (
+    <div>
+   { !isOwnerpath && <Navbar />}
+   {false &&  <HotelReg />}
+   <div className='min-h-[70vh]'>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path="/room" element={<AllRooms/>} />
+    <Route path="/room/:id" element={<RoomDetails/>} />
+    <Route path="/my-bookings" element={<MyBookings/>} />
+    <Route path='/owner' element={<Layout/>} >
+    <Route index element={<Dashboard/>}/>
+    <Route path="add-room" element={<AddRoom/>}/>
+    <Route path="list-room" element={<ListRoom/>}/>
+    </Route>
+    </Routes>
+   </div>
+   <Footer />
+   </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureDestination from '../components/FeatureDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NEWSletter'
+
+
+
+
+const Home = () => {
+  return (
+    <>
+<Hero />
+<FeatureDestination/>
+   <ExclusiveOffers/>
+   <Testimonial/>
+   <NewsLetter/>
+   
+    </>
+  )
+}
+
+export default Home
