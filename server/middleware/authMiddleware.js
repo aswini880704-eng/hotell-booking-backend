a726e57132136c562848bbe52b1a2b40d72c9be1
Aswini S <aswiniskthvl@gmail.com>
2025-10-16 12:27:45 +0530

    first commit

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,11 +2,11 @@ import User from '../models/user.js';
 // Middleware to check if user is authenticated
 
 export const protect = async (req, res, next) => {
-    const {userId} = req.auth;
-    if (!userId) {
+    const {user} = req.auth;
+    if (!user) {
         res.json({success: false, message: "Not authenticated"});
     }else{
-        const user = await User.findById(userId);
+        const user = await User.findById(user);
 req.user = user;
         next();
 }
